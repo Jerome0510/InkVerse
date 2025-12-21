82e68a6c14808ea48b589f6b49c4313c65ff6a23
Jerome Marbach <jeromemarbach@outlook.fr>
2025-12-21 23:44:24 +0100

    create home backround and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import styles from "./page.module.css";
+
 const Home = () => {
-  return;
+  return (
+    <>
+      <section className={styles.homeBackground}>
+        <img
+          className={styles.homeLogo}
+          src="/logos/InkLogoBlanc.png"
+          alt="Logo InkVerse"
+        />
+      </section>
+    </>
+  );
 };
 
 export default Home;
